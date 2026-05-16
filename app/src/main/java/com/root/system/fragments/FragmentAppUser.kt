1a2c33e4ecbb322095144172211ce381419e4d9c
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2026-05-16 19:40:41 +0800

    Gradle: set compileSdkVersion 36

--- app/src/main/java/com/root/system/fragments/FragmentAppUser.kt
+++ app/src/main/java/com/root/system/fragments/FragmentAppUser.kt
@@ -18,15 +18,16 @@ import com.root.model.AppInfo
 import com.root.ui.AdapterAppList
 import com.root.utils.AppListHelper
 import com.root.system.R
+import com.root.system.databinding.FragmentAppListBinding
 import com.root.system.dialogs.DialogAppOptions
 import com.root.system.dialogs.DialogSingleAppOptions
-import kotlinx.android.synthetic.main.fragment_app_list.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.lang.ref.WeakReference
 
 class FragmentAppUser(private val myHandler: Handler) : androidx.fragment.app.Fragment() {
+    private lateinit var binding: FragmentAppListBinding
     private lateinit var processBarDialog: ProgressBarDialog
     private lateinit var appListHelper: AppListHelper
     private var appList: ArrayList<AppInfo>? = null
@@ -34,36 +35,36 @@ class FragmentAppUser(private val myHandler: Handler) : androidx.fragment.app.Fr
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        processBarDialog = ProgressBarDialog(activity!!, "FragmentAppUser")
-        appListHelper = AppListHelper(context!!)
-
-        return inflater.inflate(R.layout.fragment_app_list, container, false)
+        processBarDialog = ProgressBarDialog(requireActivity(), "FragmentAppUser")
+        appListHelper = AppListHelper(requireContext())
+        binding = FragmentAppListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        app_list.addHeaderView(this.layoutInflater.inflate(R.layout.list_header_app, null))
+        binding.appList.addHeaderView(this.layoutInflater.inflate(R.layout.list_header_app, null))
 
         val onItemLongClick = AdapterView.OnItemLongClickListener { parent, _, position, id ->
             if (position < 1)
                 return@OnItemLongClickListener true
             val adapter = (parent.adapter as HeaderViewListAdapter).wrappedAdapter
             val app = adapter.getItem(position - 1) as AppInfo
-            DialogSingleAppOptions(activity!!, app, myHandler!!).showSingleAppOptions()
+            DialogSingleAppOptions(requireActivity(), app, myHandler!!).showSingleAppOptions()
             true
         }
 
-        app_list.onItemLongClickListener = onItemLongClick
-        fab_apps.setOnClickListener {
-            getSelectedAppShowOptions(activity!!)
+        binding.appList.onItemLongClickListener = onItemLongClick
+        binding.fabApps.setOnClickListener {
+            getSelectedAppShowOptions(requireActivity())
         }
 
         this.setList()
     }
 
     private fun getSelectedAppShowOptions(activity: Activity) {
-        var adapter = app_list.adapter
+        var adapter = binding.appList.adapter
         adapter = (adapter as HeaderViewListAdapter).wrappedAdapter
         val selectedItems = (adapter as AdapterAppList).getSelectedItems()
         if (selectedItems.size == 0) {
@@ -83,7 +84,7 @@ class FragmentAppUser(private val myHandler: Handler) : androidx.fragment.app.Fr
         GlobalScope.launch(Dispatchers.Main) {
             appList = appListHelper.getUserAppList()
             processBarDialog.hideDialog()
-            app_list?.run {
+            binding.appList?.run {
                 setListData(appList, this)
             }
         }
@@ -94,30 +95,30 @@ class FragmentAppUser(private val myHandler: Handler) : androidx.fragment.app.Fr
             return
         myHandler.post {
             try {
-                val adapterObj = AdapterAppList(context!!, dl, keywords)
+                val adapterObj = AdapterAppList(requireContext(), dl, keywords)
                 val adapterAppList: WeakReference<AdapterAppList> = WeakReference(adapterObj)
                 lv.adapter = adapterObj
                 lv.onItemClickListener = OnItemClickListener { list, itemView, postion, _ ->
                     if (postion == 0) {
-                        val checkBox = itemView.findViewById(R.id.select_state_all) as CheckBox
+                        val checkBox: CheckBox = itemView.findViewById(R.id.select_state_all)
                         checkBox.isChecked = !checkBox.isChecked
                         if (adapterAppList.get() != null) {
                             adapterAppList.get()!!.setSelecteStateAll(checkBox.isChecked)
                             adapterAppList.get()!!.notifyDataSetChanged()
                         }
                     } else {
-                        val checkBox = itemView.findViewById(R.id.select_state) as CheckBox
+                        val checkBox: CheckBox = itemView.findViewById(R.id.select_state)
                         checkBox.isChecked = !checkBox.isChecked
                         val all = lv.findViewById<CheckBox>(R.id.select_state_all)
                         if (adapterAppList.get() != null) {
                             all.isChecked = adapterAppList.get()!!.getIsAllSelected()
                         }
                     }
-                    fab_apps.visibility = if (adapterAppList.get()?.hasSelected() == true) View.VISIBLE else View.GONE
+                    binding.fabApps.visibility = if (adapterAppList.get()?.hasSelected() == true) View.VISIBLE else View.GONE
                 }
                 val all = lv.findViewById<CheckBox>(R.id.select_state_all)
                 all.isChecked = false
-                fab_apps.visibility = View.GONE
+                binding.fabApps.visibility = View.GONE
             } catch (ex: Exception) {
             }
         }
@@ -130,7 +131,7 @@ class FragmentAppUser(private val myHandler: Handler) : androidx.fragment.app.Fr
         set (value) {
             if (keywords != value) {
                 keywords = value
-                app_list?.run {
+                binding.appList?.run {
                     setListData(appList, this)
                 }
             }
