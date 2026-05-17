13559d6ca20540b11f3b8ff365a3cee52929d3a1
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2026-05-17 09:12:57 +0800

    Gradle: update to 9.5.1

--- krscript/src/main/java/com/root/krscript/downloader/FileMD5.java
+++ krscript/src/main/java/com/root/krscript/downloader/FileMD5.java
@@ -36,7 +36,7 @@ public class FileMD5 {
     }
 
     public String bytesToHexString(byte[] src) {
-        StringBuilder stringBuilder = new StringBuilder("");
+        StringBuilder stringBuilder = new StringBuilder();
         if (src == null || src.length <= 0) {
             return null;
         }
