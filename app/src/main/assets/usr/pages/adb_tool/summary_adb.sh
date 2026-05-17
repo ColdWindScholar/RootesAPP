3a0fe3ae30c59aa7823385e8ab2fffb1d666be99
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2026-05-18 00:01:15 +0800

    Use toybox insteadof busybox

--- app/src/main/assets/usr/pages/adb_tool/summary_adb.sh
+++ app/src/main/assets/usr/pages/adb_tool/summary_adb.sh
@@ -1,5 +1,5 @@
 adb devices | sed -e 's/device$/ 设备已连接/g' -e 's/offline$/ 设备已离线/g' -e 's/unauthorized$/ 没有允许授权USB调试/g'
-    Number=`adb devices | grep -v -i 'List of .*' | wc -l`
+    Number=$(adb devices | grep -v -i 'List of .*' | wc -l)
     if [[ "$Number" -gt 2 ]]; then
         echo "$a"
     fi
