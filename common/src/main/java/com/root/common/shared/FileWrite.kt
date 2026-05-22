3f6b2c861fec8cbf3862e3cb4f09943adc13b09f
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2026-05-22 19:00:25 +0800

    Use toybox insteadof busybox

--- common/src/main/java/com/root/common/shared/FileWrite.kt
+++ common/src/main/java/com/root/common/shared/FileWrite.kt
@@ -39,8 +39,10 @@ object FileWrite {
                 dir.mkdirs()
             val filePath = getPrivateFilePath(context, outName)
             val fileDir = File(filePath).parentFile
-            if (!fileDir.exists())
-                fileDir.mkdirs()
+            if (fileDir != null) {
+                if (!fileDir.exists())
+                    fileDir.mkdirs()
+            }
 
             val fileOutputStream = FileOutputStream(filePath)
 
