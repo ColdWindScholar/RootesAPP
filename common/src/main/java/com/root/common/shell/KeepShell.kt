69ac3f581355b2134068e13ec43c925077a6a543
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2026-05-22 21:59:36 +0800

    Use toybox insteadof busybox

--- common/src/main/java/com/root/common/shell/KeepShell.kt
+++ common/src/main/java/com/root/common/shell/KeepShell.kt
@@ -65,7 +65,7 @@ class KeepShell(private var rootMode: Boolean = true) {
             Log.e("doCmdSync-Lock", "线程等待超时${System.currentTimeMillis()} - $enterLockTime > $LOCK_TIMEOUT")
         }
         val builder = ProcessBuilder()
-        builder.directory(File("/data/data/com.root.system/files/"))
+        builder.directory(File("/data/data/com.root.system/files/usr/kr-script"))
         envs?.let {
             for ((key, value) in envs){
                 builder.environment()[key] = value
