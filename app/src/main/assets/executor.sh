3defcbc7871e11bccaaa373113b056eb2d5eb647
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2026-05-23 19:10:19 +0800

    Use toybox insteadof busybox

--- app/src/main/assets/executor.sh
+++ app/src/main/assets/executor.sh
@@ -1,50 +1,40 @@
-export SD_PATH=$({SDCARD_PATH})
-export EXECUTOR_PATH=$({EXECUTOR_PATH})
-export TMPDIR=$({TEMP_DIR})
-export HOME=$({START_DIR})
-export APP_USER_ID=$({APP_USER_ID})
-export SDK=$({ANDROID_SDK})
-export Package_name=$({PACKAGE_NAME})
-export Version_Name=$({PACKAGE_VERSION_NAME})
-export Version_code=$({PACKAGE_VERSION_CODE})
-export PREFIX=$({TOOLKIT})
-export Have_ROOT=$({ROOT_PERMISSION})
-export ANDROID_UID=$({ANDROID_UID})
-export LANGUAGE=zh-cn
-export DATA_DIR=${HOME%/${Package_name}*}
-export PATH0="$PATH"
-export Pages=$PREFIX/pages
-export BUSYBOX="$({BUSYBOX})"
-export ShellScript=$PREFIX/kr-script
-export PeiZhi_File=~/Configuration_File
-export Data_Dir=$PREFIX/Data_Dir
-export ELF1_Path="$PREFIX/bin"
-export ELF2_Path="$PREFIX/bin/xbin"
-export ELF3_Path="$PREFIX/xbin"
-export ELF4_Path="$PREFIX/busybox"
-export which="toybox which"
-export data_MD5="$ShellScript/init_all_MD5.sh"
-export Load="$ShellScript/init_data.sh"
-export Core="$ShellScript/Util_Functions.sh"
-export PATH="${ELF1_Path}:${ELF2_Path}:${ELF3_Path}:${PATH0}:${ELF4_Path}"
-export Bin_MT1="$DATA_DIR/bin.mt.plus/files/term/usr/bin"
-export Bin_MT2="$DATA_DIR/bin.mt.plus/files/term/usr/lib"
-export Bin_MT3="$DATA_DIR/bin.mt.plus.canary/files/term/usr/bin"
-export Bin_MT4="$PREFIX"
-export TMP=/data/local/tmp
+SD_PATH=$({SDCARD_PATH})
+EXECUTOR_PATH=$({EXECUTOR_PATH})
+TMPDIR=$({TEMP_DIR})
+HOME=$({START_DIR})
+APP_USER_ID=$({APP_USER_ID})
+SDK=$({ANDROID_SDK})
+Package_name=$({PACKAGE_NAME})
+Version_Name=$({PACKAGE_VERSION_NAME})
+Version_code=$({PACKAGE_VERSION_CODE})
+PREFIX=$({TOOLKIT})
+Have_ROOT=$({ROOT_PERMISSION})
+ANDROID_UID=$({ANDROID_UID})
+LANGUAGE=zh-cn
+DATA_DIR=${HOME%/${Package_name}*}
+PATH0="$PATH"
+Pages=$PREFIX/pages
+BUSYBOX="$({BUSYBOX})"
+ShellScript=$PREFIX/kr-script
+PeiZhi_File=~/Configuration_File
+Data_Dir=$PREFIX/Data_Dir
+ELF1_Path="$PREFIX/bin"
+ELF2_Path="$PREFIX/bin/xbin"
+ELF3_Path="$PREFIX/xbin"
+ELF4_Path="$PREFIX/busybox"
+which="toybox which"
+data_MD5="$ShellScript/init_all_MD5.sh"
+Load="$ShellScript/init_data.sh"
+Core="$ShellScript/Util_Functions.sh"
+PATH="${ELF1_Path}:${ELF2_Path}:${ELF3_Path}:${PATH0}:${ELF4_Path}"
+Bin_MT1="$DATA_DIR/bin.mt.plus/files/term/usr/bin"
+Bin_MT2="$DATA_DIR/bin.mt.plus/files/term/usr/lib"
+Bin_MT3="$DATA_DIR/bin.mt.plus.canary/files/term/usr/bin"
+Bin_MT4="$PREFIX"
+TMP=/data/local/tmp
 
-export START_DIR="$({START_DIR})"
-export TEMP_DIR="$({TEMP_DIR})"
-export ANDROID_SDK="$({ANDROID_SDK})"
-export MAGISK_PATH="$({MAGISK_PATH})"
+START_DIR="$({START_DIR})"
+TEMP_DIR="$({TEMP_DIR})"
+ANDROID_SDK="$({ANDROID_SDK})"
+MAGISK_PATH="$({MAGISK_PATH})"
 
-[[ -f $Core ]] && . $Core
-
-if [[ -f "$1" ]]; then
-    cd "$ShellScript"
-    . "$@" &
-else
-    echo "${1} 脚本已丢失！"
-fi
-wait
-exit 0
