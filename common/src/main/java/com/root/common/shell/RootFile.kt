481a1eda12d041aedaf4fdd5144726d578f30e28
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2026-05-24 12:05:53 +0800

    Use toybox insteadof busybox

--- common/src/main/java/com/root/common/shell/RootFile.kt
+++ common/src/main/java/com/root/common/shell/RootFile.kt
@@ -21,7 +21,7 @@ object RootFile {
     }
 
     fun dirExists(path: String): Boolean {
-        return KeepShellPublic.doCmdSync("if [[ -d \"$path\" ]]; then echo 1; fi;").equals("1")
+        return KeepShellPublic.doCmdSync("if [[ -d \"$path\" ]]; then echo 1; fi;") == "1"
     }
 
     fun deleteDirOrFile(path: String) {
@@ -30,10 +30,10 @@ object RootFile {
 
     // 通过MD5比对两个文件是否相同
     fun fileEquals(path1: String, path2: String): Boolean {
-        if (path1.equals(path2)) {
+        if (path1 == path2) {
             return true
         }
-        return KeepShellPublic.doCmdSync("if [[ -f \"$path1\" ]] && [[ -f \"$path2\" ]]; then\nif [[ `md5sum -b \"$path1\"` = `md5sum -b \"$path2\"` ]]; then\n echo 1\nfi\nfi").equals("1")
+        return KeepShellPublic.doCmdSync("if [[ -f \"$path1\" ]] && [[ -f \"$path2\" ]]; then\nif [[ `md5sum -b \"$path1\"` = `md5sum -b \"$path2\"` ]]; then\n echo 1\nfi\nfi") == "1"
     }
 
     // 处理像 "drwxrwx--x   3 root     root         4096 1970-07-14 17:13 vendor_de/" 这样的数据行
@@ -47,10 +47,10 @@ object RootFile {
 
             val columns = row.trim().split(" ")
             val size = columns[0]
-            file.fileSize = size.toLong() * 1024;
+            file.fileSize = size.toLong() * 1024
 
             //  8 /data/adb/modules/scene_systemless/ => /data/adb/modules/scene_systemless/
-            val fileName = row.substring(row.indexOf(size) + size.length + 1);
+            val fileName = row.substring(row.indexOf(size) + size.length + 1)
 
             if (fileName == "./" || fileName == "../") {
                 return null
