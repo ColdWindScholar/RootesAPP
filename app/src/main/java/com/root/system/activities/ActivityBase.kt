5d7f4b409f604f92c7515fb7ad453c3db47b8007
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2026-05-30 21:07:01 +0800

    Use toybox insteadof busybox

--- app/src/main/java/com/root/system/activities/ActivityBase.kt
+++ app/src/main/java/com/root/system/activities/ActivityBase.kt
@@ -1,7 +1,6 @@
 package com.root.system.activities
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
