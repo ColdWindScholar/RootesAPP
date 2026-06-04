7ccbb1a37a25c3e6f6e042ce65b0190729cba9b0
ColdWindScholar <140364844+ColdWindScholar@users.noreply.github.com>
2026-06-04 14:12:32 +0800

    Use toybox insteadof busybox

--- app/src/main/assets/usr/kr-script/Zipsigner.sh
+++ app/src/main/assets/usr/kr-script/Zipsigner.sh
@@ -6,15 +6,15 @@ version=1
 jian="$ELF1_Path/zipsigner"
 log="$Data_Dir/Zipsigner_version.log"
 
-[[ -f $log ]] && user_version=`cat $log` || user_version=0
+[[ -f $log ]] && user_version=$(cat "$log") || user_version=0
 
 if [[ $user_version -lt $version || ! -f $jian ]]; then
     echo $version >"$log"
     Choice=1
-    . $Load Zipsigner
+    . "$Load" Zipsigner
     echo "开始解压资源……"
     unzip -oq "$Download_File" -d "$ELF1_Path"
-    chmod 755 $ELF1_Path/*
+    chmod 755 "$ELF1_Path"/*
 fi
 
 
@@ -23,10 +23,10 @@ File1=${File%.*}
 File2=${File##*.}
 File3="${File1}_sign.$File2"
 PATH=$ELF1_Path:$PATH
-chmod 755 $ELF1_Path/*
+chmod 755 "$ELF1_Path"/*
 ! test -f "$File" && abort "！$File 文件不存在"
 echo "签名中请骚等，速度看文件大小而定……"
-rm -rf $ELF1_Path/dalvikvm
+rm -rf "$ELF1_Path"/dalvikvm
 zipsigner "$File" "$File3"
     if [[ $? = 0 ]]; then
         echo "文件已保存到：$File3"
